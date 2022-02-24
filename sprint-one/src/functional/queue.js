b340b49b336171fe123e3e558e30338ee2e793f0
Ryan De Leon <ryandeleon0@gmail.com>
2022-02-24 13:03:16 -0800

    complete functional stack, partially functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function() {
   var someInstance = {};
+  var length = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +8,22 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.dequeue = function() {
+    if (length > 0) {
+      // remove last entry
+      delete storage[length - 1];
+      // subtract 1 from length
+      length--;
+      // return storage;
+    }
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,30 @@
 var Stack = function() {
   var someInstance = {};
+  var length = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.pop = function() {
+    if (length > 0) {
+      var item = storage[length - 1];
+      delete storage[length - 1];
+      length--;
+      return item;
+    }
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
 };
+
+
