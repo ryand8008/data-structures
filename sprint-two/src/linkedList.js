71edc6e4b1051af21db405aabfbb556d71eab71d
Ryan De Leon <ryandeleon0@gmail.com>
2022-02-26 12:18:42 -0800

    passed all tree tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,35 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+
   list.addToTail = function(value) {
+    var newNode = Node(value);
+
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      // change old tail next pointer to newNode
+      list.tail.next = newNode;
+      //change tail to the newNode
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var item = list.head.value;
+    list.head = list.head.next;
+    return item;
   };
 
   list.contains = function(target) {
+    for (let key in list) {
+      if (list[key].value === target) {
+        return true;
+      }
+
+    }
+    return false;
   };
 
   return list;
@@ -23,6 +45,7 @@ var Node = function(value) {
 
   return node;
 };
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
